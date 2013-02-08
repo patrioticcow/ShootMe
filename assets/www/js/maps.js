693bca9dc5f9eec6325390b2c645cf2898a043ba
patrioticcow <cristi.catea@gmail.com>
2013-02-08 14:38:25 -0800

    0.32

--- assets/www/js/maps.js
+++ assets/www/js/maps.js
@@ -5,18 +5,23 @@ var Maps = (function () {
     }
 
     Maps.prototype.getWatchCoord = function () {
-        var options = { enableHighAccuracy: true, timeout: 4000 };
+        var options = { enableHighAccuracy: true, timeout: 30000 };
         navigator.geolocation.watchPosition(this.onWatchSuccess, this.onWatchError, options);
     };
 
 
     Maps.prototype.onWatchSuccess = function (position) {
         // temporary, emulate movement
-        var movement = Math.floor(Math.random() * 9) + 1;
+        var movement = (Math.floor(Math.random() * 5) + 1) * 0.0001;
 
         // add to local storage
-        window.localStorage.setItem('latitude'          , position.coords.latitude + movement );
-        window.localStorage.setItem('longitude'         , position.coords.longitude + movement );
+        window.localStorage.setItem('latitude'          , position.coords.latitude );
+        window.localStorage.setItem('longitude'         , position.coords.longitude );
+
+        window.localStorage.setItem('latitude_other'          , position.coords.latitude + movement );
+        window.localStorage.setItem('longitude_other'         , position.coords.longitude + movement );
+
+
         window.localStorage.setItem('altitude'          , position.coords.altitude );
         window.localStorage.setItem('accuracy'          , position.coords.accuracy );
         window.localStorage.setItem('altitudeAccuracy'  , position.coords.altitudeAccuracy );
@@ -28,44 +33,81 @@ var Maps = (function () {
         console.log('code: '    + error.code    + '\n' + 'message: ' + error.message + '\n');
     };
 
+    Maps.prototype.initialize = function (latitude, longitude, z, o) {
 
-    Maps.prototype.initialize = function () {
-        var latitude = window.localStorage.getItem("latitude");
-        var longitude = window.localStorage.getItem("longitude");
+        this.center    = new google.maps.LatLng(latitude, longitude);
+        this.zoom      = z;
+        this.node      = document.getElementById(o);
+        this.markers   = [];
 
         var mapOptions = {
-            zoom: 8,
-            center: new google.maps.LatLng(latitude, longitude),
-            mapTypeId: google.maps.MapTypeId.ROADMAP
+            zoom:       this.zoom,
+            center:     this.center,
+            mapTypeId:  google.maps.MapTypeId.ROADMAP
         };
 
-        var map = new google.maps.Map(document.getElementById('map_canvas'),  mapOptions);
+        this.map = new google.maps.Map(this.node,  mapOptions);
 
-        var marker = new google.maps.Marker({
-            position: map.getCenter(),
-            map: map,
-            title: 'Click to zoom'
-        });
-
-        return marker;
-
-        // 10 seconds after the center of the map has changed, pan back to the marker.
-        /*
-        google.maps.event.addListener(map, 'center_changed', function() {
-            window.setTimeout(function() {
-                map.panTo(marker.getPosition());
-            }, 10000);
-        });
-        */
+        return this;
     };
 
-    Maps.prototype.changePosition = function () {
+    Maps.prototype.addMarker = function(latitude, longitude, t, c, markerImage){
 
-        var latitude = window.localStorage.getItem("latitude");
-        var longitude = window.localStorage.getItem("longitude");
+        var image = new google.maps.MarkerImage(markerImage,
+            new google.maps.Size(53, 52)
+        );
 
-        var map = new google.maps.LatLng(latitude, longitude);
-        marker.setPosition(map);
+        var marker = new google.maps.Marker({
+            position: new google.maps.LatLng(latitude, longitude),
+            map: this.map,
+            title: t || 'Click to zoom',
+            icon: image
+        });
+
+        this.markers.push(marker);
+
+        if(c){
+            this.map.setCenter(marker.getPosition());
+        }
+        return marker;
+    }
+
+    Maps.prototype.addCircle = function(marker, rad, latitude, longitude, f) {
+
+        this.center    = new google.maps.LatLng(latitude, longitude);
+
+        var circle = new google.maps.Circle({
+            center: this.center,
+            radius: rad,
+            strokeColor: "#FF0000",
+            strokeOpacity: 0.5,
+            strokeWeight: 1,
+            fillColor: "#FF0000",
+            fillOpacity: 0.1,
+            map: this.map
+        });
+
+        circle.bindTo('center', marker, 'position');
+
+        // run callback
+        if(f){
+            f(rad);
+        }
+    };
+
+    Maps.prototype.changePosition = function (marker, lat, long, c, f) {
+        var latLng = new google.maps.LatLng(lat, long);
+
+        marker.setPosition(latLng);
+
+        if(c){
+            this.map.setCenter(latLng);
+        }
+
+        // run callback
+        if(f){
+            f(lat, long);
+        }
     }
 
     return Maps;
--- assets/www/js/script.js
+++ assets/www/js/script.js
@@ -5,14 +5,50 @@ function init() {
 var watchID = null;
 
 function deviceReady() {
-    // to do
-    var maps = new Maps();
-    maps.initialize();
 
-    window.setTimeout(function() {
-        var maps = new Maps();
-        maps.changePosition();
-    }, 3000);
+    //create the Maps-instance
+    var maps = new Maps();
+    var latitude = window.localStorage.getItem("latitude");
+    var longitude = window.localStorage.getItem("longitude");
+    var friendLatitude = window.localStorage.getItem("latitude_other");
+    var friendLongitude = window.localStorage.getItem("longitude_other");
+
+    //initialize maps
+    maps.initialize(latitude, longitude, 20, 'map_canvas');
+
+    //add a marker
+    var marker = maps.addMarker(latitude, longitude, 'me', true, 'images/markers/m1.png');
+    var friendMarker = maps.addMarker(friendLatitude, friendLongitude, 'friend', false, 'images/markers/m2.png');
+
+    //radius
+    var radMe = 0.010 * 1600; // miles in meters
+    var radFriend = 0.003 * 1600; // miles in meters
+    maps.addCircle(marker, radMe, latitude, longitude, function(rad){
+        console.log('rad ' + rad);
+    });
+
+    maps.addCircle(friendMarker, radFriend, friendLatitude, friendLongitude, function(rad){
+        console.log('rad ' + rad);
+    });
+
+    //change position of marker
+    window.setInterval(function() {
+
+        var latitude = window.localStorage.getItem("latitude");
+        var longitude = window.localStorage.getItem("longitude");
+        var friendLatitude = window.localStorage.getItem("latitude_other");
+        var friendLongitude = window.localStorage.getItem("longitude_other");
+
+        maps.changePosition(marker, latitude, longitude, true, function(lat, long){
+            console.log('position changed to ' + lat + " " + long);
+        });
+
+        maps.changePosition(friendMarker, friendLatitude, friendLongitude, false, function(lat, long){
+            console.log('friend position changed to ' + lat + " " + long);
+        });
+
+    }, 10000);
+
 }
 
 $( document ).bind( "mobileinit", function() {
